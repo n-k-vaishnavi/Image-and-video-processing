181e0fe98d69f2d0b9cdbb9f8097f0684b994a78
n-k-vaishnavi <vaishnavijee2023@gmail.com>
2025-09-13 18:53:33 +0530

    More basics

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
 print("Welcome to the meme world!")
+name = "Vaishnavi"
+print("Hello " + name + " Your meme is loading!")
+for i in range(5):
+    print("Meme #" + str(i+1))
+def repeat_word(word, n):
+    return (word + " ") * n
+
+print(repeat_word("LOL", 3))
+def make_laugh(name1):
+    print("We made "+ name1 + " laugh LOL!")
+make_laugh("Vaishnavi")
